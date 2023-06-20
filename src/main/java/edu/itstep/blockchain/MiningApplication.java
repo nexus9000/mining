527026bf74c1ee588e187504a755c8515e6e4306
User <java.user.tester@gmail.com>
2023-06-20 19:59:08 +0300

    init project

---
+++ src/main/java/edu/itstep/blockchain/MiningApplication.java
@@ -0,0 +1,13 @@
+package edu.itstep.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiningApplication.class, args);
+	}
+
+}
