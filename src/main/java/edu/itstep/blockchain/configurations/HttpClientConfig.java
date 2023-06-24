b46d012f517a1d8b9106703070f6e54b5f2cd5b6
User <java.user.tester@gmail.com>
2023-06-24 11:04:08 +0300

    http client settings

--- src/main/java/edu/itstep/blockchain/configurations/HttpClientConfig.java
+++ src/main/java/edu/itstep/blockchain/configurations/HttpClientConfig.java
@@ -37,18 +37,14 @@ public class HttpClientConfig {
 
 	private static final int MAX_TOTAL_CONNECTIONS = 20;
 	private static final TimeValue DEFAULT_KEEP_ALIVE_TIME_MILLIS = TimeValue.ofMilliseconds(20_000);
-	
 
 	@Bean
 	public ConnectionKeepAliveStrategy connectionKeepAlive() {
 		return new ConnectionKeepAliveStrategy() {
-
-			
-
+         //comments
 			@Override
 			public TimeValue getKeepAliveDuration(org.apache.hc.core5.http.HttpResponse response, HttpContext context) {
-				BasicHeaderElementIterator it = new BasicHeaderElementIterator(
-						response.headerIterator());
+				BasicHeaderElementIterator it = new BasicHeaderElementIterator(response.headerIterator());
 				while (it.hasNext()) {
 					HeaderElement he = it.next();
 					String param = he.getName();
@@ -62,43 +58,38 @@ public class HttpClientConfig {
 
 		};
 	}
+
 	@Bean
 	public CloseableHttpClient httpClient() {
-		RequestConfig requestConfig = RequestConfig.custom()
-				.setConnectionRequestTimeout(REQUEST_TIMEOUT)
-				.setConnectionRequestTimeout(CONNECT_TIMEOUT)
-				.build();
-		return HttpClients.custom()
-				.setDefaultRequestConfig(requestConfig)
-				.setConnectionManager(poolingConnectionManager())
-				.setKeepAliveStrategy(connectionKeepAlive())
-				.build();
+		RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(REQUEST_TIMEOUT)
+				.setConnectionRequestTimeout(CONNECT_TIMEOUT).build();
+		return HttpClients.custom().setDefaultRequestConfig(requestConfig)
+				.setConnectionManager(poolingConnectionManager()).setKeepAliveStrategy(connectionKeepAlive()).build();
 	}
-	
+
 	@Bean
-    public PoolingHttpClientConnectionManager poolingConnectionManager() {
-        SSLContextBuilder builder = new SSLContextBuilder();
-        try {
-            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-        } catch (NoSuchAlgorithmException | KeyStoreException e) {
-            LOGGER.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
-        }
- 
-        SSLConnectionSocketFactory sslsf = null;
-        try {
-            sslsf = new SSLConnectionSocketFactory(builder.build());
-        } catch (KeyManagementException | NoSuchAlgorithmException e) {
-            LOGGER.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
-        }
- 
-        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
-                .<ConnectionSocketFactory>create().register("https", sslsf)
-                .register("http", new PlainConnectionSocketFactory())
-                .build();
- 
-        PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-        poolingConnectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
-        return poolingConnectionManager;
-    }
- 
+	public PoolingHttpClientConnectionManager poolingConnectionManager() {
+		SSLContextBuilder builder = new SSLContextBuilder();
+		try {
+			builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+		} catch (NoSuchAlgorithmException | KeyStoreException e) {
+			LOGGER.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
+		}
+
+		SSLConnectionSocketFactory sslsf = null;
+		try {
+			sslsf = new SSLConnectionSocketFactory(builder.build());
+		} catch (KeyManagementException | NoSuchAlgorithmException e) {
+			LOGGER.error("Pooling Connection Manager Initialisation failure because of " + e.getMessage(), e);
+		}
+
+		Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("https", sslsf).register("http", new PlainConnectionSocketFactory()).build();
+
+		PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager(
+				socketFactoryRegistry);
+		poolingConnectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
+		return poolingConnectionManager;
+	}
+
 }
