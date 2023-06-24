dd2a4276c6b3463df13ef425c5908dc7094ddfc4
User <java.user.tester@gmail.com>
2023-06-24 12:06:37 +0300

    http client settings

--- src/main/java/edu/itstep/blockchain/MiningApplication.java
+++ src/main/java/edu/itstep/blockchain/MiningApplication.java
@@ -1,7 +1,9 @@
 package edu.itstep.blockchain;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MiningApplication {
@@ -10,4 +12,10 @@ public class MiningApplication {
 		SpringApplication.run(MiningApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run() {
+		return (String[] arg) -> {
+
+		};
+	}
 }
---
+++ src/main/java/edu/itstep/blockchain/domain/BlockPersistent.java
@@ -0,0 +1,36 @@
+package edu.itstep.blockchain.domain;
+
+import java.util.List;
+
+import edu.itstep.blockchain.mining.Transaction;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="blocks")
+@Getter@Setter@NoArgsConstructor
+public class BlockPersistent {
+	
+	   @Id
+	   @GeneratedValue(strategy = GenerationType.IDENTITY)
+	   private Long id;
+	   
+	   private int nonce;
+	   
+	   @Column(name="time_stamp")
+	   private long timeStamp;
+	   
+	   private String hash;
+	   
+	   @Column(name="previous_hash")
+	   private String previousHash;
+	   
+	   //public List<Transaction> transactions;
+}
